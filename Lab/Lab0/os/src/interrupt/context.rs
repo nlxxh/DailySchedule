b859aba4e55a8a8e06a7367fa64d518966138fe8
Nlxxh <2631089844@qq.com>
2020-07-16 18:57:48 +0800

    first commit

--- Lab/Lab0/os/src/interrupt/context.rs
+++
@@ -1,8 +0,0 @@
-use riscv::register::sstatus::Sstatus;
-#[repr(C)]
-pub struct Context{
-    pub x:[usize;32],
-    pub sstatus:Sstatus,
-    pub sepc:usize,
-}
-
--- Lab/Lab0/os/src/interrupt/mod.rs
+++
@@ -1,8 +0,0 @@
-mod handler;
-mod context;
-
-pub fn init(){
-    handler::init();
-    println!("mod interrupt initialized");
-
-}
